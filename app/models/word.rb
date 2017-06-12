29ff178706fd057ac359312989fd27bb958696a8
Sophia Pignatelli <pignatelli@fordham.edu>
2017-06-12 17:42:37 -0400

    test tutnese

--- app/models/word.rb
+++ app/models/word.rb
@@ -41,27 +41,29 @@ def sub_letters(letter)
   'y' => 'yub',
   'z' => 'zub'
   )
-
   return translate_letter
 end
 
-def tutnese_dialect_one(word)
-  word_array = word.scan(/((\w)\2*)/).map(&:first)
+def tutnese_dialect_one
+
+  word_array = self.english.scan(/((\w)\2*)/).map(&:first)
   translate_array = word_array.collect{|i| i.length >1 && i==="ee" || i==="ii"  ||  i==="uu"? ["squat"] +  [i] : i.length>1 && i === "aa" || i === "oo" ? ["squat"] + [i[0] + "h" ]:  i.length >1? ["squa"] + [i] : i}.flatten!
-  i = 0
-  while i < translate_array.length
-    if translate_array[i].length == 1
-      if translate_array[i]=~ /[^aeiou]/
-        translate_array[i] = sub_letters(translate_array[i])
-      else 
-        translate_array[i] = translate_array[i]
-      end
-    else
-      translate_array[i] = translate_array[i]
-    end
-    i +=1
-  end
-  return translate_array.join("")
+  if translate_array
+	  i = 0
+	  while i < translate_array.length
+	    if translate_array[i].length == 1
+	      if translate_array[i]=~ /[^aeiou]/
+	        translate_array[i] = sub_letters(translate_array[i])
+	      else 
+	        translate_array[i] = translate_array[i]
+	      end
+	    else
+	      translate_array[i] = translate_array[i]
+	    end
+	    i +=1
+	  end
+	  return translate_array.join("")
+	end
 end
 
 end
