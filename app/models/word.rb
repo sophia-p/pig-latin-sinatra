67092eadb1393e9fe05db8411526d0b878e11e56
Sophia Pignatelli <pignatelli@fordham.edu>
2017-06-12 17:29:04 -0400

    add tutnese logic

--- app/models/word.rb
+++ app/models/word.rb
@@ -16,5 +16,51 @@ class Word < ActiveRecord::Base
 	end
   end
 
+def tutnese_dialect_one
+	def sub_letters(letter)
+  translate_letter = letter.gsub!(/[bcdfghjklmnpqrstvwxyz]/,
+  'b' => 'bub',
+  'c' => 'cash',
+  'd' => 'dud',
+  'f' => 'fuf',
+  'g' => 'gug',
+  'h' => 'hash',
+  'j' => 'jay',
+  'k' => 'kuck',
+  'l' => 'lul',
+  'm' => 'mum',
+  'n' => 'nun',
+  'p' => 'pub',
+  'q' => 'quack',
+  'r' => 'rug',
+  's' => 'sus',
+  't' => 'tut',
+  'v' => 'vuv',
+  'w' => 'wack',
+  'x' => 'ex',
+  'y' => 'yub',
+  'z' => 'zub'
+  )
 
+  return translate_letter
+end
+
+def tutnese_dialect_one(word)
+  word_array = word.scan(/((\w)\2*)/).map(&:first)
+  translate_array = word_array.collect{|i| i.length >1 && i==="ee" || i==="ii"  ||  i==="uu"? ["squat"] +  [i] : i.length>1 && i === "aa" || i === "oo" ? ["squat"] + [i[0] + "h" ]:  i.length >1? ["squa"] + [i] : i}.flatten!
+  i = 0
+  while i < translate_array.length
+    if translate_array[i].length == 1
+      if translate_array[i]=~ /[^aeiou]/
+        translate_array[i] = sub_letters(translate_array[i])
+      else 
+        translate_array[i] = translate_array[i]
+      end
+    else
+      translate_array[i] = translate_array[i]
+    end
+    i +=1
+  end
+  return translate_array.join("")
+end
 end
