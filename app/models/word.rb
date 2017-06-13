0c9a1cf5e66cc2571780d1235450ab5de2fcf58e
Sophia Pignatelli <pignatelli@fordham.edu>
2017-06-12 23:25:54 -0400

    add oppish logic

--- app/models/word.rb
+++ app/models/word.rb
@@ -17,53 +17,68 @@ class Word < ActiveRecord::Base
   end
 
 
-def sub_letters(letter)
-  translate_letter = letter.gsub!(/[bcdfghjklmnpqrstvwxyz]/,
-  'b' => 'bub',
-  'c' => 'cash',
-  'd' => 'dud',
-  'f' => 'fuf',
-  'g' => 'gug',
-  'h' => 'hash',
-  'j' => 'jay',
-  'k' => 'kuck',
-  'l' => 'lul',
-  'm' => 'mum',
-  'n' => 'nun',
-  'p' => 'pub',
-  'q' => 'quack',
-  'r' => 'rug',
-  's' => 'sus',
-  't' => 'tut',
-  'v' => 'vuv',
-  'w' => 'wack',
-  'x' => 'ex',
-  'y' => 'yub',
-  'z' => 'zub'
-  )
-  return translate_letter
-end
+  def sub_letters(letter)
+    translate_letter = letter.gsub!(/[bcdfghjklmnpqrstvwxyz]/,
+    'b' => 'bub',
+    'c' => 'cash',
+    'd' => 'dud',
+    'f' => 'fuf',
+    'g' => 'gug',
+    'h' => 'hash',
+    'j' => 'jay',
+    'k' => 'kuck',
+    'l' => 'lul',
+    'm' => 'mum',
+    'n' => 'nun',
+    'p' => 'pub',
+    'q' => 'quack',
+    'r' => 'rug',
+    's' => 'sus',
+    't' => 'tut',
+    'v' => 'vuv',
+    'w' => 'wack',
+    'x' => 'ex',
+    'y' => 'yub',
+    'z' => 'zub'
+    )
+    return translate_letter
+  end
 
-def tutnese_dialect_one
+  def tutnese_dialect_one
 
-  word_array = self.english.scan(/((\w)\2*)/).map(&:first)
-  translate_array = word_array.collect{|i| i.length >1 && i==="ee" || i==="ii"  ||  i==="uu"? ["squat"] +  [i] : i.length>1 && i === "aa" || i === "oo" ? ["squat"] + [i[0] + "h" ]:  i.length >1? ["squa"] + [i] : i}.flatten!
-  if translate_array
-	  i = 0
-	  while i < translate_array.length
-	    if translate_array[i].length == 1
-	      if translate_array[i]=~ /[^aeiou]/
-	        translate_array[i] = sub_letters(translate_array[i])
-	      else 
-	        translate_array[i] = translate_array[i]
-	      end
-	    else
-	      translate_array[i] = translate_array[i]
-	    end
-	    i +=1
-	  end
-	  return translate_array.join("")
-	end
+    word_array = self.english.scan(/((\w)\2*)/).map(&:first)
+    translate_array = word_array.collect{|i| i.length >1 && i==="ee" || i==="ii"  ||  i==="uu"? ["squat"] +  [i] : i.length>1 && i === "aa" || i === "oo" ? ["squat"] + [i[0] + "h" ]:  i.length >1? ["squa"] + [i] : i}
+    if translate_array
+  	  i = 0
+  	  while i < translate_array.length
+  	    if translate_array[i].length == 1
+  	      if translate_array[i]=~ /[^aeiou]/
+  	        translate_array[i] = sub_letters(translate_array[i])
+  	      else 
+  	        translate_array[i] = translate_array[i]
+  	      end
+  	    else
+  	      translate_array[i] = translate_array[i]
+  	    end
+  	    i +=1
+  	  end
+  	  return translate_array.join("")
+  	end
+  end
+
+def oppish
+  word = self.english.split("")
+  i = 0
+  word_array = []
+  while i < word.length
+    if word[i] =~ /[^aeiou]/
+      word_array << [word[i]] + ["opp"] 
+    else 
+      word_array << [word[i]]
+    end
+  i +=1
+  end
+  return word_array.flatten!.join("")
 end
 
 end
